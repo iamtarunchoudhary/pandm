cc3f7274ba16a6e9a4e994b21680b3ff44327722
tarun.choudhary <tarun.choudhary@voicetree.co>
2020-02-10 22:53:04 +0530

    initali setup

--- package_and_movers/wsgi.py
+++ package_and_movers/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for package_and_movers project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'package_and_movers.settings')
 
 application = get_wsgi_application()
